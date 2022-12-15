904f0c34f1866ae15d150aebd84a04d59435edee
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-15 21:49:36 +0100

    Add files via upload

---
+++ file decryption .py
@@ -0,0 +1,18 @@
+def decrypt(self, ciphertext):
+        try:
+            with open(ciphertext, 'rb') as src:
+                with open(ciphertext.strip(".enc") + ".dec", 'wb+') as des:
+                    iv = src.read(16)
+                    cipher = AES.new(self.K4, AES.MODE_GCM, iv)
+                    for block in iter(lambda: src.read(AES.block_size * 128), b''):
+                        des.write(cipher.decrypt(block))
+
+                    # Remove padding
+                    # Set the pos to the beginning of the last byte
+                    des.seek(-1, os.SEEK_END)
+                    # Read the last byte
+                    last = des.read(1)
+                    des.seek(-int.from_bytes(last, byteorder='big'), os.SEEK_END)
+                    des.truncate()
+        except (IOError, OSError):
+            raise IOError("Cannot open the file to decrypt")
---
+++ file encryption .py
@@ -0,0 +1,20 @@
+def encrypt(self, filename):
+    iv = get_random_bytes(16)
+    cipher = AES.new(self.K4, AES.MODE_GCM, iv)
+    try:
+        with open(filename, 'rb') as src:
+            with open(filename + ".enc", 'wb') as des:
+                des.write(iv)
+                for block in iter(lambda: src.read(AES.block_size * 128), b''):
+                    if len(block) == AES.block_size * 128:
+                        des.write(cipher.encrypt(block))
+
+                    # Padding the last block
+                    else:
+                        remain = len(block) % 16
+                        if remain == 0:
+                            remain = 16
+                        block += utility.to_bytes((chr(remain) * remain))
+                        des.write(cipher.encrypt(block))
+    except (IOError, OSError):
+        raise IOError("Cannot open the file to encrypt")
