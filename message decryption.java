c748d66622b146c1450bb0cfdacce0a39b5d75c5
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-15 21:39:28 +0100

    Add files via upload

---
+++ message decryption.java
@@ -0,0 +1,6 @@
+
+/* Decrypt the message, given derived key and initialization vector. */
+Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+String plaintext = new String(cipher.doFinal(ciphertext), StandardCharsets.UTF_8);
+System.out.println(plaintext);
