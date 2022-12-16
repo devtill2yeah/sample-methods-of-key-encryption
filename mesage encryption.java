ad8c40b7ccef3872da6c1052bff536072aea79e4
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-16 02:16:52 +0100

    Update mesage encryption.java

--- mesage encryption.java
+++ mesage encryption.java
@@ -5,3 +5,5 @@ cipher.init(Cipher.ENCRYPT_MODE, secret);
 AlgorithmParameters params = cipher.getParameters();
 byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
 byte[] ciphertext = cipher.doFinal("Hello, World!".getBytes(StandardCharsets.UTF_8));
+
+
